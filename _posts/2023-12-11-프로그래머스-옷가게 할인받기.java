0db815de9cc7222203345aa77d0ad33e46eabd56
eunchae <119996957+eunchaelyu@users.noreply.github.com>
2023-12-11 23:50:01 +0900

    Update 2023-12-11-프로그래머스-옷가게 할인받기.java

--- _posts/2023-12-11-프로그래머스-옷가게 할인받기.java
+++ _posts/2023-12-11-프로그래머스-옷가게 할인받기.java
@@ -1,4 +1,15 @@
-class Solution {
+---
+title: 프로그래머스 Lv.0 옷가게 할인 받기
+author: eunchaelyu
+date: 2023-12-11 22:00:00 +09:00
+categories: [성장 일기, 1일 1커밋, 프로그래머스]
+tags: [프로그래머스, 1일 1커밋, 성장일기]
+pin: true
+img_path: '/posts/20231211'
+---
+  
+  ```java
+  class Solution {
     public int solution(int price) {
         if(price>=500000){
             price*=0.80;
@@ -10,3 +21,4 @@ class Solution {
         return price;
     }
 }
+```
